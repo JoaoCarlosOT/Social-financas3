939992d5ecb103f8283c7ef9dce3270cb62fdd4f
Escobarly <escobarlyamarall@gmail.com>
2024-04-28 16:27:02 -0300

    Adicionado equipes a serem exibidas, reestruturado cor do modo dark.

--- Js/equipe.js
+++ Js/equipe.js
@@ -3,6 +3,11 @@ const mudarTemaEscuroEquipe = () => {
     body.style.background = '#2D2B2B';
     body.style.color = 'white';
 
+    var letters = document.querySelectorAll('.selectionseach');
+    letters.forEach(ttext => {
+        ttext.style.color = 'white';
+    });
+
     var header = document.getElementsByTagName("header")[0];
     header.style.background = '#10482E';
 
@@ -27,6 +32,63 @@ const mudarTemaEscuroEquipe = () => {
         ttext.style.color = 'white';
     });
     areatext.forEach(atext => {
-        atext.style.color = '#5AF0A9';
+        atext.style.color = '#1AA744';
     });
 };
+
+const section = () => {
+    var team1 = document.querySelector('.team1');
+    var team2 = document.querySelector('.team2');
+    var team3 = document.querySelector('.team3');
+    var team4 = document.querySelector('.team4');
+    var team5 = document.querySelector('.team5');
+    setTimeout(() => {
+        var sections = document.querySelectorAll('.selectionboxs');
+        sections.forEach(section => {
+            if(section.value == 1){
+                if(section.checked){
+                    team1.style.display = "flex";
+                }else{
+                    team1.style.display = "none";
+                }
+            }
+            if(section.value == 2){
+                if(section.checked){
+                    team2.style.display = "flex";
+                }else{
+                    team2.style.display = "none";
+                }
+            }
+            if(section.value == 3){
+                if(section.checked){
+                    team3.style.display = "flex";
+                }else{
+                    team3.style.display = "none";
+                }
+            }
+            if(section.value == 4){
+                if(section.checked){
+                    team4.style.display = "flex";
+                }else{
+                    team4.style.display = "none";
+                }
+            }
+            if(section.value == 5){
+                if(section.checked){
+                    team5.style.display = "flex";
+                }else{
+                    team5.style.display = "none";
+                }
+            }
+        });
+    }, 10);
+}
+
+var team1 = document.querySelector('.team1');
+var sections = document.querySelectorAll('.selectionboxs');
+sections.forEach(section => {
+    if(section.value == 1){
+        section.checked;
+        team1.style.display = "flex";
+    }
+});
