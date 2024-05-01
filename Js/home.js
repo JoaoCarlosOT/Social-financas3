c8e56ecbaf66ab2a53f4cc3d49377a5da3b1fa69
JoaocarlosOT <joaocarlosdev403@gmail.com>
2024-05-01 19:09:34 -0300

    modo-dark home finalizado

--- Js/home.js
+++ Js/home.js
@@ -19,42 +19,58 @@ const fecharMenu = () => {
 };
 
 const mudarTemaEscuro = () => {
+    var detailsList = document.querySelectorAll(".details");
+
+    detailsList.forEach((detail) => {
+        detail.classList.toggle('dark');
+        detail.classList.toggle('title-dark');
+    });
+
     var tagWhite = document.querySelector('.box');
-    tagWhite.style.background = '#2D2B2B';
+    tagWhite.classList.toggle('dark');
 
     var section = document.querySelector('#curiosidades');
-    section.style.background = '#2D2B2B';
+    section.classList.toggle('dark');
 
     var titleSection = document.querySelector('.title-curiosidades');
-    titleSection.style.color = 'white';
+    titleSection.classList.toggle('title-dark');
 
     var tagWhite = document.querySelector('.title-box');
-    tagWhite.style.background = '#2D2B2B';
-    tagWhite.style.color = 'white';
+    tagWhite.classList.toggle('dark');
+    tagWhite.classList.toggle('title-dark');
 
     var article = document.getElementsByTagName("article")[0];
-    article.style.background = '#10482E';
+    article.classList.toggle('dark-verde');
 
     var header = document.getElementsByTagName("header")[0];
-    header.style.background = '#10482E';
+    header.classList.toggle('dark-verde');
 
     var button = document.getElementsByTagName("button")[0];
-    button.style.background = '#fff';
-
-    var iconsun = document.querySelector('#icon-sun');
-    iconsun.style.display = 'inline';
-
-    var iconmoon = document.querySelector('#icon-moon');
-    iconmoon.style.display = 'none';
-
-    var footer = document.getElementsByTagName("footer")[0];
-    footer.style.background = '#10482E';
+    button.classList.toggle('button-white');
 
     var p = document.querySelector('.p-footer');
-    p.style.background = '#10482E';
+    p.classList.toggle('dark-verde');
+
+    var footer = document.getElementsByTagName("footer")[0];
+    footer.classList.toggle('dark-verde');
+
+
+    document.body.classList.toggle('dark');
+
+    const iconMoon = document.querySelector('#icon-moon');
+    const iconSun = document.querySelector('#icon-sun');
+
+    const isDarkMode = document.body.classList.contains('dark');
+
+    iconMoon.style.display = isDarkMode ? 'none' : 'inline';
+    iconSun.style.display = isDarkMode ? 'inline' : 'none';
+    
+    
 };
 
-addEventListener("click", mudarTema);
+document.getElementById("toggle-navbar").addEventListener("click", mudarTemaEscuro);
 
+
+document.querySelector('.button').addEventListener("click", mudarTemaEscuro);
 document.querySelector('.abrir-btn').addEventListener("click", abrirMenu);
 document.querySelector('.fechar-btn').addEventListener("click", fecharMenu);
